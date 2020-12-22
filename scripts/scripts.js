af837628c617665eec775ef86fcf9e10a2b24618
Aleksandr Boblovskiy <developers3@gmail.com>
2020-12-22 20:19:50 +0300

    Исправлены баги

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,41 @@
-let EditButton = document.querySelector('.profile__EditButton')
-let popup = document.querySelector('.popup')
-let closeButton = document.querySelector('.popup__close')
-let form = document.querySelector('.popup__container')
-let titleName = document.querySelector('.profile__title')
-let popupName = document.querySelector('.popup__name')
-let subName = document.querySelector('.profile__subtitle')
-let popupSubName = document.querySelector('.popup__subname')
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close');
+let form = document.querySelector('.popup__container');
+let titleName = document.querySelector('.profile__title');
+let username = document.querySelector('.popup__name');
+let subName = document.querySelector('.profile__subtitle');
+let occupation = document.querySelector('.popup__subname');
+let like = document.querySelectorAll('.elements__like');
 
 function tooglePopup() {
-  popup.classList.toggle('popup_active')
+  popup.classList.toggle('popup_active');
 };
 
-EditButton.addEventListener('click', () => {
-  tooglePopup()
-  popupName.value = titleName.textContent
-  popupSubName.value = subName.textContent
+function openPopup() {
+  tooglePopup();
+  username.value = titleName.textContent;
+  occupation.value = subName.textContent;
+}
+
+function savePopup(event) {
+  event.preventDefault();
+  titleName.textContent = username.value;
+  subName.textContent = occupation.value;
+  tooglePopup();
+}
+
+popup.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget)
+  tooglePopup();
 });
 
-closeButton.addEventListener('click', tooglePopup)
-
-popup.addEventListener('click', (Event) => {
-  if (Event.target === Event.currentTarget)
-  tooglePopup()
-})
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  titleName.textContent = popupName.value
-  subName.textContent = popupSubName.value
-  tooglePopup()
-})
-
-let like = document.querySelectorAll('.elements__like')
+closeButton.addEventListener('click', tooglePopup);
+editButton.addEventListener('click', openPopup);
+form.addEventListener('submit', savePopup);
 
 like.forEach((value) => {
   value.addEventListener('click', () => {
-    value.classList.toggle('elements__like_active')
-  })
-})
+    value.classList.toggle('elements__like_active');
+  });
+});
