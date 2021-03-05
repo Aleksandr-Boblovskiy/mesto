8169fb22e9c027c6b33a2f03258792faf2ca3889
Aleksandr Boblovskiy <developers3@gmail.com>
2021-03-05 14:49:55 +0300

    Сделать класс PopupWithImg PopupWithForm

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,6 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
-
     this._popup = document.querySelector(this._popupSelector);
   }
 
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,33 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popup.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+      //  this._popup.reset();
+    })
+  }
+
+  _getInputValues() {
+    this._inputList = this._popup.querySelectorAll('.form__input');
+    this._formValues = {};
+    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+
+    return this._formValues;
+  }
+
+  close() {
+    super.close();
+    this._popup.reset();
+  }
+
+
+
+}
